589a4af87548f0650f2052182d8aeb89c804318f
Ruediger Goebl <r.goebl@tum.de>
2016-01-03 16:07:28 +0100

    Initial commit

---
+++ polydivisible/PPDN.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace polydivisible
+{
+    // potential polidivisible number
+    class PPDN
+    {
+        public PPDN(byte _base)
+        {
+            m_base = _base;
+            m_nextBase = 0;
+            m_nextBaseRemainder = 0;
+            m_digits = new byte[_base - 1];
+            m_digitsLeft = new List<byte>();
+
+            m_lastBases = new Stack<BigInteger>(_base);
+            m_lastBaseRemainders = new Stack<int>(_base);
+
+            m_digitsUpToNow = 0;
+
+            for (byte d = 1; d < _base; d++)
+            {
+                m_digitsLeft.Add(d);
+            }
+        }
+
+        public PPDN(PPDN other)
+        {
+            m_base = other.m_base;
+            m_nextBase = other.m_nextBase;
+            m_nextBaseRemainder = other.m_nextBaseRemainder;
+
+            m_lastBases = new Stack<BigInteger>(other.m_lastBases);
+            m_lastBaseRemainders = new Stack<int>(other.m_lastBaseRemainders);
+
+            m_digits = (byte[])other.m_digits.Clone();
+            m_digitsUpToNow = other.m_digitsUpToNow;
+        }
+
+        public IList<byte> digitsLeft()
+        {
+            return m_digitsLeft.AsReadOnly();
+        }
+
+        public bool addDigit(byte digit)
+        {
+            if (m_digitsUpToNow == m_base - 1)
+                return false;
+
+            if (m_digitsUpToNow == 0)
+            {
+                m_digits[0] = digit;
+                m_nextBase = digit * m_base;
+                m_digitsUpToNow = 1;
+
+                m_lastBases.Push(new BigInteger(0));
+                m_lastBaseRemainders.Push(0);
+
+                m_nextBaseRemainder = (int)(m_nextBase % (m_digitsUpToNow + 1));
+
+                m_digitsLeft.Remove(digit);
+                return true;
+            }
+            else
+            {
+                int divisor = m_digitsUpToNow + 1;
+                bool divisible = ((m_nextBaseRemainder + digit) % divisor == 0);
+
+                if (divisible)
+                {
+                    m_digits[m_digitsUpToNow] = digit;
+                    m_digitsUpToNow++;
+                    
+                    m_lastBases.Push(m_nextBase);
+                    m_lastBaseRemainders.Push(m_nextBaseRemainder);
+                    
+                    m_nextBase = (m_nextBase + digit) * m_base;
+                    m_nextBaseRemainder = (int)(m_nextBase % (m_digitsUpToNow + 1));
+
+                    m_digitsLeft.Remove(digit);
+                }
+                return divisible;
+            }
+        }
+
+        public void removeLastDigit()
+        {
+            if (m_digitsUpToNow >= 1)
+            {
+                byte digitToRemove = m_digits[m_digitsUpToNow-1];
+                m_digitsUpToNow--;
+                //m_nextBase = (m_nextBase  / m_base - digitToRemove);
+                //m_nextBaseRemainder = (int)(m_nextBase % (m_digitsUpToNow + 1));
+                m_nextBase = m_lastBases.Pop();
+                m_nextBaseRemainder = m_lastBaseRemainders.Pop();
+
+                m_digitsLeft.Add(digitToRemove);
+            }
+        }
+
+        public override string ToString()
+        {
+            string desc = "<";
+            for (int position = 0; position < m_digitsUpToNow; position++)
+            {
+                desc += m_digits[position].ToString() + ", ";
+            }
+            desc += ">";
+            return desc;
+        }
+
+        private byte m_base;
+        private byte[] m_digits;
+        private byte m_digitsUpToNow;
+        private List<byte> m_digitsLeft;
+        private BigInteger m_nextBase;
+        private int m_nextBaseRemainder;
+        private Stack<BigInteger> m_lastBases;
+        private Stack<int> m_lastBaseRemainders;
+    }
+}
---
+++ polydivisible/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace polydivisible
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte bStart = 3;
+            if (args.Length >= 1)
+            {
+                byte.TryParse(args[0], out bStart);
+            }
+
+            for (byte b = bStart; b <= 255; b++)
+            {
+                tryBase(b);
+            }
+            
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                Console.WriteLine("Press enter to close...");
+                Console.ReadLine();
+            }
+        }
+
+        static void tryBase(byte b)
+        {
+            Console.Out.WriteLine("Base " + b + " (" + DateTime.Now.ToString() + ")");
+            Parallel.For(1, b,
+                   index =>
+                   {
+                       byte dInner = (byte)index;
+                       PPDN p = new PPDN(b);
+                       tryDigit(p, dInner, (byte)(b - 1));
+                       Console.Out.WriteLine("    " + dInner);
+                   });
+            //for (byte d1 = 1; d1 < b; d1++)
+            //{
+            //    PPDN p = new PPDN(b);
+            //    tryDigit(p, d1, (byte)(b - 1));
+            //}
+        }
+
+        static void tryDigit(PPDN p, byte digit, byte levelsLeft)
+        {
+            bool worked = p.addDigit(digit);
+            if (worked)
+            {
+                if (levelsLeft - 1 > 0)
+                {
+                    List<byte> digitsToTry = new List<byte>(p.digitsLeft());
+                    foreach (byte nextDigit in digitsToTry)
+                    {
+                        tryDigit(p, nextDigit, (byte)(levelsLeft - 1));
+                    }
+                }
+                else
+                {
+                    Console.Out.WriteLine("  FOUND! " + p);
+                }
+                p.removeLastDigit();
+            }
+        }
+    }
+}
